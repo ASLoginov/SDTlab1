796685cd7289aca2265c24fa89429b6677e95b9b
Alexander Loginov <kipling.live@gmail.com>
2024-03-19 16:47:39 +0700

    Base class logic and Counter class added

--- win.h
+++ win.h
@@ -1,14 +1,37 @@
-#ifndef WIN_H
-#define WIN_H
+#ifndef win_h
+#define win_h
+#include <QtWidgets>
 
-#include <QMainWindow>
-
-class win : public QMainWindow
+class Counter:public QLineEdit
 {
     Q_OBJECT
-
 public:
-    win(QWidget *parent = nullptr);
-    ~win();
+    Counter(const QString & contents, QWidget *parent=0):
+    QLineEdit(contents,parent){}
+signals:
+    void tick_signal();
+public slots:
+    void add_one()
+    {
+        QString str=text();
+        int r=str.toInt();
+        if (r!=0 && r%5 ==0) emit tick_signal();
+        r++;
+        str.setNum(r);
+        setText(str);
+    }
 };
-#endif // WIN_H
+
+class win: public QWidget
+{
+    Q_OBJECT
+protected:
+    QLabel *label1,*label2;
+    Counter *edit1,*edit2;
+    QPushButton *calcbutton;
+    QPushButton *exitbutton;
+public:
+    win(QWidget *parent = 0);
+};
+#endif
+
